b374730fe440ff0cacd2b5d88f6148658041dc55
Make Magazin <mail@make-magazin.de>
2024-03-21 17:52:07 +0100

    Add files via upload

---
+++ Beispielprojekt/Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\avr\microchipstudio\7.0\Packs\atmel\ATtiny_DFP\1.10.348\include/avr/iotn212.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\xmega.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\avr\microchipstudio\7.0\Packs\atmel\ATtiny_DFP\1.10.348\include/avr/iotn212.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\xmega.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\avr\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Beispielprojekt/main.c
@@ -0,0 +1,48 @@
+/*
+* Florian Schäffer
+* https://www.blafusel.de
+*
+* 1/2024
+* ATtiny Demo mit DAC
+*/
+
+#define F_CPU 3333333UL		// 3,3 MHz (OSCCFG = 20 Mhz, MCLKCTRLB = 0x8 => Prescaler 6)
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	PORTA.DIRSET = (1 << 3);					// Pin 3 im Richtungsregister auf 1 = Output 
+	PORTA.DIRCLR = (1 << 2);					// Pin 2 auf 1 = als Eingang
+	PORTA.PIN2CTRL = PORT_PULLUPEN_bm;			// Pullup für Pin 2 von Port A aktivieren; Bit 3 altern.: (1<<3)  
+	
+	VREF.CTRLA |= VREF_DAC0REFSEL_4V34_gc;       // Voltage reference auf 4.34V
+	_delay_us(25);	// 25 µs warten lt. Datenblatt
+	// The DAC output pin needs to have the digital input buffer and the pull-up resistor disabled to reduce its load:
+	PORTA.PIN6CTRL &= ~PORT_ISC_gm;
+	PORTA.PIN6CTRL |= PORT_ISC_INPUT_DISABLE_gc;
+	PORTA.PIN6CTRL &= ~PORT_PULLUPEN_bm;
+	DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm | DAC_RUNSTDBY_bm;		// Enabling DAC, Output Buffer (sends the OUT signal to a pin) and Run in Standby sleep mode
+	
+	uint8_t DACwert = 0;
+
+	while (1)
+	{
+		if (PORTA.IN & (1 << 2))		// PA2 High? = Taster offen
+		{
+			PORTA.OUTCLR |= (1<<3);		// PB3 LED LOW (aus)
+			_delay_ms(150);
+		}
+		else
+		{
+			PORTA.OUTSET |= (1<<3);		// PB3 LED HIGH (ein)
+			DACwert += 5;							// Variable um 5 erhöhen
+			DAC0.DATA = DACwert;			// DAC-Wert ausgeben
+			_delay_ms(50);
+			PORTA.OUTCLR |= (1<<3);		// PB3 LED LOW
+			_delay_ms(50);
+		}
+	}
+}
+
